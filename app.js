664d79cee93d668a17ffef7f15138860afae27d6
+ <+>
2024-11-22 15:55:13 +0300

    adds section and javascript

---
+++ app.js
@@ -0,0 +1,17 @@
+function showModal(modalId) {
+    document.getElementById(modalId).style.display = "flex";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close modal if clicked outside the modal content
+window.onclick = function(event) {
+    const modals = document.querySelectorAll('.modal');
+    modals.forEach(modal => {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    });
+};
